7ee3e67e44d1eea3f9ae161663c17e8f078c7dff
sanengineer <48153104+sanengineer@users.noreply.github.com>
2020-08-31 05:12:27 +0700

    ganti tampilan UI

---
+++ js/app.js
@@ -0,0 +1,25 @@
+// const humanChooseRock = function humanRockFuntion() {
+//   return document.getElementById("humanChoose").getAttribute("value");
+// };
+
+// const humanChoosePaper = function humanPaperFuntion() {
+//   return document.getElementById("humanChoose").getAttribute("value");
+// };
+// const humanChooseScissors = function humanScissorsFuntion() {
+//   return document.getElementById("humanChoose").getAttribute("value");
+// };
+
+// const humanChooseRock = document
+//   .getElementById("humanChoose")
+//   .getAttribute("value");
+
+// const humanChoosePaper = document
+//   .getElementById("humanChoose")
+//   .getAttribute("value");
+// const humanChooseScissors = document
+//   .getElementById("humanChoose")
+//   .getAttribute("value");
+
+// console.log(humanChooseRock);
+// console.log(humanChoosePaper);
+// console.log(humanChooseScissors);
---
+++ js/game-board-2.js
@@ -0,0 +1,4 @@
+let humanScore = document.querySelector("#humanScore");
+let botScore = document.querySelector("#botScore");
+let winLoseDrawStatus = document.querySelector("#winLoseDrawStatus");
+let humanButtonChoose = document.querySelectorAll(".human-button-choose");
